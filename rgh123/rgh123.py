42258cc69838efad176b91542f9758ebdd8eb065
würthless elektroniks <wurthlesselektroniks@proton.me>
2025-06-24 19:19:43 -0400

    rgh123: document 10 MHz timings

--- rgh123/rgh123.py
+++ rgh123/rgh123.py
@@ -54,7 +54,10 @@ def resetter():
     wait(0, pin, 0)                       # 0xDA
     label("9")
     jmp(y_dec, "9")                       # 9
-    set(pindirs, 1)                   [3] # 10
+    set(pindirs, 1)                  [31] # 10
+    nop() [31]
+    nop() [31]
+    nop() [31]
     set(pins, 1)                          # 11
     set(pindirs, 0)                       # 12
     push(noblock)
@@ -91,8 +94,6 @@ def init_sm(reset_assert_delay):
     else:
         raise RuntimeError("cannot set I/O drive...")
 
-
-
     # reset delay is around 26.924 ms in RGH3
     # that's about 1292352 cycles @ 48 MHz
     # actual working or plausible delay value not found yet
@@ -107,14 +108,20 @@ def init_sm(reset_assert_delay):
 
 
 def do_reset_glitch() -> int:
+    CPU_PLL_BYPASS.value(0)
     i2c = SoftI2C(sda=Pin(8),scl=Pin(9),freq=100000)
     while (mem32[RP2040_GPIO_IN] & POST_BITS_MASK) != POST_D5:
         pass 
     while (mem32[RP2040_GPIO_IN] & POST_BITS_MASK) != POST_D6:
         pass
 
-    print("0xD6 arrived, start PIO")
     pio_sm.active(1)
+
+    CPU_PLL_BYPASS.value(1)
+    i2c.writeto_mem(0x70, 0xCE, bytearray([0x04, 0x14, 0x44, 0xE8, 0x28]))
+    CPU_PLL_BYPASS.value(0)
+
+    print("0xD6 arrived")
 
     # default clock is [ 0x14, 0x44, 0xE8, 0x08 ]
     # 27 MHz mode is [ 0x14, 0x44, 0xE8, 0x28 ]
@@ -124,6 +131,7 @@ def do_reset_glitch() -> int:
 
     ticks_DA = 0
     ticks_after_DA = ()
+    time_pio_finished = 0
 
     try:
         while True:
@@ -131,30 +139,27 @@ def do_reset_glitch() -> int:
             t = ticks_us()
             this_post = (v >> 15) & 0xFF
 
-            if last_post != 0xD8 and this_post == 0xD8:
-                CPU_PLL_BYPASS.value(1)
-                i2c.writeto_mem(0x70, 0xCE, bytearray([0x04, 0x14, 0x44, 0xE8, 0x28]))
-                CPU_PLL_BYPASS.value(0)
+            # if last_post != 0xD6 and this_post == 0xD6:
 
             if last_post != 0xD9 and this_post == 0xD9:
+                # 0.035 for 27 MHz, 0.202-0.206 for 10 MHz
                 sleep(0.035)
                 CPU_PLL_BYPASS.value(1)
 
             if last_post != 0xDA and this_post == 0xDA:
                 ticks_DA = t
-
-                # block until PIO is done
-                # print("awaiting PIO finish...")
                 pio_sm.get()
-                print("PIO is done")
+                time_pio_finished = ticks_us()
+
+                CPU_PLL_BYPASS.value(0)
+                i2c.writeto_mem(0x70, 0xCE, bytearray([0x04, 0x14, 0x44, 0xE8, 0x08]))
             
             if this_post != last_post:
                 print(f"{this_post:02x}")
                 last_post = this_post
                 if this_post == 0xDB:
                     print("got candidate!!!")
-                    i2c.writeto_mem(0x70, 0xCE, bytearray([0x04, 0x14, 0x44, 0xE8, 0x08]))
-                    CPU_PLL_BYPASS.value(0)
+
 
             if this_post == 0x00:
                 print("FAIL: SMC timed out")
@@ -168,7 +173,7 @@ def do_reset_glitch() -> int:
                 #
                 # note also that if your reset wait value is too high, it will
                 # cause this result to be skewed upwards.
-                print(f"-> DA -> F2 = {t-ticks_DA} usec")
+                print(f"-> DA -> F2 = {t-ticks_DA} usec. pio {t-time_pio_finished} usec")
                 return 1
     finally:
         i2c.writeto_mem(0x70, 0xCE, bytearray([0x04, 0x14, 0x44, 0xE8, 0x08]))
@@ -185,6 +190,9 @@ def do_reset_glitch_loop():
     # this does get CB_B executing, but then the CPU crashes at 0x22.
     # occasionally weird POST codes show up (0x88, 0x44, etc.)
     #
+    # 10 MHz timings:
+    # - 0xDA -> 0xF2 transition at approx 150-155 ms
+    #
     reset_trial = 1292328
     while True:
         print(f"start trial of: {reset_trial}")
@@ -196,5 +204,5 @@ def do_reset_glitch_loop():
         if result == 2:
             init_sm(0)
             return
-        elif result != 0:
-            reset_trial += 1
+        # elif result != 0:
+        reset_trial -= 1
